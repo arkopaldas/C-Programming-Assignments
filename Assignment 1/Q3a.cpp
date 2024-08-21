3825791e13c6970a0f16862aa686de6cd9c9f936
arkopaldas <123320941+arkopaldas@users.noreply.github.com>
2024-08-21 12:42:08 +0530

    Update Q3a.cpp

--- Assignment 1/Q3a.cpp
+++ Assignment 1/Q3a.cpp
@@ -5,6 +5,13 @@ int main() {
     char vowel1='a',vowel2='e',vowel3='i',vowel4='o',vowel5='u';
     char capVowel1='A',capVowel2='E',capVowel3='I',capVowel4='O',capVowel5='U';
     int v21 = vowel2-vowel1, v52 = vowel5-vowel2, cv21 = capVowel2-capVowel1;
+    
+    /* Yes all the expressions are valid expressions since the difference in 
+    character gets stored as the difference in their integer ASCII values.
+    
+    The difference between 'a' and 'e' is same as the difference 
+    between 'A' and 'E' according to their ASCII values. */
+    
     cout << "vowel2-vowel1 : " << v21 << endl;
     cout << "vowel5-vowel2 : " << v52 << endl;
     cout << "capVowel2-capVowel1 : " << cv21 << endl;
