7f653eea7d578c236593ab9905c1c8d515817110
arkopaldas <123320941+arkopaldas@users.noreply.github.com>
2024-10-30 21:27:34 +0530

    Update Q2.cpp

--- Assignment 7/Q2.cpp
+++ Assignment 7/Q2.cpp
@@ -23,9 +23,9 @@ int main() {
         Tiny c3 = c2 - c1; // c3 = 60;
         Tiny c4 = c3; // no range check required
         int i = int(c1) + int(c2); // i = 64
-        // c1 = c1 + c2; // range error: c1 can’t be 64
+        c1 = c1 + c2; // range error: c1 can’t be 64
         i = int(c3) - 64; //i = -4
-        // c2 = c3 - 64; // range error: c2 can’t be -4
+        c2 = c3 - 64; // range error: c2 can’t be -4
         c3 = c4; // no range check required
     }catch (Tiny::Bad_range&) {
         cerr << "Range error occurred!" << endl;
